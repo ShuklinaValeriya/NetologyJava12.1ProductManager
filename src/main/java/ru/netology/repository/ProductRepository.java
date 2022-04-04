6a4f2cd11c86587a36fa1cb262bde66c06f18fca
unknown <shuklinavaleria@yandex.ru>
2022-04-04 13:05:45 +0300

    change code and add tests

--- src/main/java/ru/netology/repository/ProductRepository.java
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -29,6 +29,8 @@ public class ProductRepository {
             }
         }
         products = tmp;
+
     }
 
 }
+//
--- src/main/java/ru/netology/ru/netology/manager/ProductManager.java
+++ src/main/java/ru/netology/ru/netology/manager/ProductManager.java
@@ -8,19 +8,18 @@ import ru.netology.repository.ProductRepository;
 public class ProductManager {
     private ProductRepository repository;
 
-     public ProductManager(ProductRepository repository) {
+    public ProductManager(ProductRepository repository) {
         this.repository = repository;
     }
-//    this.repository
 
     public void add(Product product) {
         repository.save(product);
     }
 
-    public Product[] searchBy(String title) {
+    public Product[] searchBy(String text) {
         Product[] result = new Product[0];
-       for (Product product : repository.findAll()) {
-            if (matches(product, title)) {
+        for (Product product : repository.findAll()) {
+            if (matches(product, text)) {
                 Product[] tmp = new Product[result.length + 1];
                 System.arraycopy(result, 0, tmp, 0, result.length);
                 tmp[tmp.length - 1] = product;
@@ -51,7 +50,7 @@ public class ProductManager {
             }
             return false;
         }
-
         return false;
     }
 }
+//
